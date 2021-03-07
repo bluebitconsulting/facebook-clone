41f341569e1b01c01e12ddde36736603d77231fd
BlueBit Networking Services <76871387+bluebitconsulting@users.noreply.github.com>
2021-03-07 16:00:55 +0430

    deploying

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -6,9 +6,9 @@ function StoryReel() {
   return (
     <div className="storyReel">
       <Story
-        image="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
-        profileSrc="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
-        title="Abbas Ataie"
+        image="https://media-exp1.licdn.com/dms/image/C4E03AQG1GoCvy2Mvtg/profile-displayphoto-shrink_200_200/0/1601560005559?e=1620864000&v=beta&t=WH_uSNd_m-Cf2wWK3vioQxcbJpbHd4E73cRoDVWg2u0"
+        profileSrc="https://media-exp1.licdn.com/dms/image/C4E03AQG1GoCvy2Mvtg/profile-displayphoto-shrink_200_200/0/1601560005559?e=1620864000&v=beta&t=WH_uSNd_m-Cf2wWK3vioQxcbJpbHd4E73cRoDVWg2u0"
+        title="Ibrahim Sadaat"
       />
       <Story
         image="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
@@ -16,24 +16,24 @@ function StoryReel() {
         title="Abbas Ataie"
       />
       <Story
-        image="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
-        profileSrc="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
-        title="Abbas Ataie"
+        image="https://specials-images.forbesimg.com/imageserve/5f3c43f344884ad1395da4f5/960x0.jpg?fit=scale"
+        profileSrc="https://specials-images.forbesimg.com/imageserve/5f3c43f344884ad1395da4f5/960x0.jpg?fit=scale"
+        title="Jeff Bezus"
       />
       <Story
-        image="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
-        profileSrc="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
-        title="Abbas Ataie"
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/President_of_Afghanistan%2C_Dr._Mohammad_Ashraf_Ghani%2C_at_Hyderabad_House%2C_in_New_Delhi_on_September_19%2C_2018_%28cropped%29.JPG/330px-President_of_Afghanistan%2C_Dr._Mohammad_Ashraf_Ghani%2C_at_Hyderabad_House%2C_in_New_Delhi_on_September_19%2C_2018_%28cropped%29.JPG"
+        profileSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/President_of_Afghanistan%2C_Dr._Mohammad_Ashraf_Ghani%2C_at_Hyderabad_House%2C_in_New_Delhi_on_September_19%2C_2018_%28cropped%29.JPG/330px-President_of_Afghanistan%2C_Dr._Mohammad_Ashraf_Ghani%2C_at_Hyderabad_House%2C_in_New_Delhi_on_September_19%2C_2018_%28cropped%29.JPG"
+        title="Ashraf Ghani"
       />
       <Story
-        image="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
-        profileSrc="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
-        title="Abbas Ataie"
+        image="https://image.cnbcfm.com/api/v1/image/106806377-1607090600215-gettyimages-1229893101-AFP_8WA6E2.jpeg?v=1613170197&w=1400&h=950"
+        profileSrc="https://image.cnbcfm.com/api/v1/image/106806377-1607090600215-gettyimages-1229893101-AFP_8WA6E2.jpeg?v=1613170197&w=1400&h=950"
+        title="Elon Mask"
       />
       <Story
-        image="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
-        profileSrc="https://avatars.githubusercontent.com/u/49268657?s=460&v=4"
-        title="Abbas Ataie"
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/330px-President_Barack_Obama.jpg"
+        profileSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/330px-President_Barack_Obama.jpg"
+        title="Obama"
       />
     </div>
   )
